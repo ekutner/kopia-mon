453cc76b3c92e4af9144c76bd847d20cc80ce3f2
Eran Kutner <5628151+ekutner@users.noreply.github.com>
2024-12-19 10:20:22 +0200

    Automatically add host name to the snapshot if more than 1 host in repo

--- repoinfo.py
+++ repoinfo.py
@@ -5,6 +5,7 @@ import os
 import platform
 import typing
 import dateutil.parser
+from typing import Self
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from kopiaapi import KopiaApi
@@ -25,8 +26,9 @@ class RepoInfo:
     sources:list[dict] = field(default_factory=lambda: list())
     last_modified_file:FileInfo = None
     should_render:bool = False
+    hosts:set[str] = field(default_factory=lambda: set())
 
-    def _update_last_modified_file(self):
+    def _update_last_modified_file(self) -> None:
         hostname = platform.node().lower()
 
         for source in self.sources:
@@ -38,7 +40,7 @@ class RepoInfo:
                     self.last_modified_file.path = file_path
                     self.last_modified_file.time = file_time
 
-    def _update(self):
+    def _update(self) -> Self:
         if len(self.snapshots) == 0 and (datetime.now(tz=timezone.utc) - self.last_snapshot).total_seconds() > self.config["inactivity_days"]*3600*24:
             self.inactivity_error = True
             if self.config["validate_inactivity"]:
@@ -52,7 +54,7 @@ class RepoInfo:
         return self
 
     @classmethod
-    def create(cls, repo_conf:dict, min_time=None):
+    def create(cls, repo_conf:dict, min_time=None) -> Self:
         repo = RepoInfo(config=repo_conf)
 
         kopia = KopiaApi(repo_conf["config-file"])
@@ -63,6 +65,7 @@ class RepoInfo:
             source = snapshot["source"]
             if not any(s["host"]==source["host"] and s["path"]==source["path"] for s in repo.sources ):
                 repo.sources.append(source)
+            repo.hosts.add(source["host"])
             start_time = dateutil.parser.isoparse(snapshot["startTime"])
             if start_time > repo.last_snapshot:
                 repo.last_snapshot = start_time
